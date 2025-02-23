b5dbeeb5d25b57e27d5c2ab117059de79109b1ad
wendellast <wendelalves01a@hotmail.com>
2025-02-22 23:57:51 -0300

    New struct code

--- validgen.go
+++ validgen.go
@@ -3,18 +3,36 @@ package validgen
 import "github.com/Hublastt/ValidGen/cmd/cpf"
 
 /*
-GenerateCpf - Generate a valid cpf
-  - @return {string, error} - cpf, error
-*/
-func GenerateCpf() (string, error) {
-	return cpf.CPFFunctions{}.GenerateCPF()
-}
-
-/*
 ValidateCpf - Validate cpf is valid
   - @param {string} - cpf
   - @return {error} - error
 */
-func ValidateCpf(cpfString string) error {
-	return cpf.CPFFunctions{}.IsValid(cpfString)
+func ValidateCPF(cpfString string) error {
+	return cpf.CPFFunctions{}.ValidateCPF(cpfString)
+}
+
+/*
+GenerateCpf - Generate a valid cpf
+  - @return {string, error} - cpf, error
+*/
+func GenerateCPF() (string, error) {
+	return cpf.CPFFunctions{}.GenerateCPF()
+}
+
+/*
+FormatCpf - Format cpf
+  - @param {string} - cpf
+  - @return {string, error} - cpf, error
+*/
+func FormatCPF(cpfString string) (string, error) {
+	return cpf.CPFFunctions{}.FormatCPF(cpfString)
+}
+
+/*
+UnformatCpf - Unformat cpf
+  - @param {string} - cpf
+  - @return {string, error} - cpf, error
+*/
+func UnformatCPF(cpfString string) (string, error) {
+	return cpf.CPFFunctions{}.UnformatCPF(cpfString)
 }
--- validgen_test.go
+++ validgen_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestGenerateCpf(t *testing.T) {
-	cpf, err := GenerateCpf()
+	cpf, err := GenerateCPF()
 	if err != nil {
 		t.Errorf("GenerateCpf() error = %v", err)
 	}
@@ -18,11 +18,11 @@ func TestValidateCpf(t *testing.T) {
 	validCpf := "12345678909"
 	invalidCpf := "12345678900"
 
-	if err := ValidateCpf(validCpf); err != nil {
+	if err := ValidateCPF(validCpf); err != nil {
 		t.Errorf("ValidateCpf(%v) error = %v; want nil", validCpf, err)
 	}
 
-	if err := ValidateCpf(invalidCpf); err == nil {
+	if err := ValidateCPF(invalidCpf); err == nil {
 		t.Errorf("ValidateCpf(%v) error = nil; want error", invalidCpf)
 	}
 }
